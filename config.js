38ac201501268575946cc0193bca24fbea808dbf
Theo Vesque <theovesque171201@gmail.com>
2024-07-06 13:37:49 +0200

    Environnement variables

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: process.env.REACT_APP_API_URL || "http://127.0.0.1:8000",
+  apiUrl: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000",
 };
 
 export default config;
