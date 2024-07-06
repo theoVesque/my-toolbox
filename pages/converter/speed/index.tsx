b725132b79f888e55f1a85fee0443abfd81266ae
Theo Vesque <theovesque171201@gmail.com>
2024-07-06 13:27:21 +0200

    Modification config

--- pages/converter/speed/index.tsx
+++ pages/converter/speed/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import config from "../../../config/config";
+import config from "../../../config";
 
 const SpeedConverterForm = () => {
   const [value, setValue] = useState("");
--- pages/converter/time/index.tsx
+++ pages/converter/time/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import config from "../../../config/config";
+import config from "../../../config";
 
 const TimeConverterForm = () => {
   const [value, setValue] = useState("");
